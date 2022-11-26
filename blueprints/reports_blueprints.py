2d33e965ecd0d9456dd06dbbb7acfa219576a944
Camilozume <114374109+Camilozume@users.noreply.github.com>
2022-11-25 19:55:47 -0500

    Update reports_blueprints.py

--- blueprints/reports_blueprints.py
+++ blueprints/reports_blueprints.py
@@ -1,5 +1,22 @@
 from flask import Blueprint
 from controllers.reports_controller import ReportsController
 
+from controllers.reports_controller import ReportsController
+
 reports_blueprints = Blueprint('reports_blueprints', __name__)
 reports_controller = ReportsController()
+
+@reports_blueprints.route("/reports/voto/sorted_candidato", methods=["GET"])
+def report_sorted_candidato():
+    response = reports_controller.get_sorted_candidato()
+    return response, 200
+
+@reports_blueprints.route('/reports/voto/sorted_partido/<string:mesa_id>', methods=["GET"])
+def report_sorted_partido_by_mesa(mesa_id: str):
+    response = reports_controller.get_sorted_partido_by_mesa(mesa_id)
+    return response, 200
+
+@reports_blueprints.route('/reports/voto/partido/porcentual', methods=["GET"])
+def report_sorted_partido_porcentual():
+    response = reports_controller.get_porcentual_partido()
+    return response, 200
