5334704cc0e31598cc62bbae65b93f62d1892394
Camilozume <114374109+Camilozume@users.noreply.github.com>
2022-11-25 19:52:25 -0500

    Update reports_controller.py

--- controllers/reports_controller.py
+++ controllers/reports_controller.py
@@ -9,18 +9,8 @@ class ReportsController():
         print("Candidatos por mesa ordenado por inscritos")
         return self.reports_repository.get_sorted_candidato()
 
-    def get_sorted_candidato_by_mesa(self, mesa_id: str) -> list:
-        print("Candidatos por mesa determinada")
-        return self.reports_repository.get_sorted_candidato_by_mesa(mesa_id)
-
-    def get_sorted_partido(self) -> list:
-        return self.reports_repository.get_sorted_partido()
-
     def get_sorted_partido_by_mesa(self, mesa_id: str) -> list:
         return self.reports_repository.get_sorted_partido_by_mesa(mesa_id)
-
-    def get_sorted_mesa(self) -> list:
-        return self.reports_repository.get_sorted_mesa()
 
     def get_porcentual_partido(self) -> list:
         return self.reports_repository.get_porcentual_partidos()
